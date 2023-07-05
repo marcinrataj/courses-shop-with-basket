6aefc48ab3b0990d12afbda3f237f31ad0bb4fae
Marcin Rataj <marcinrataj0@gmail.com>
2023-07-05 09:37:06 +0200

    move some part from html to js file and adding first function

--- js/app.js
+++ js/app.js
@@ -1,25 +1,33 @@
-const product1 = { price: 10 }
-const product2 = { price: "20" }
+const product1 = { price: 10, title: 'JS od podstaw' };
+const product2 = { price: '20', title: 'PHP od podstaw' };
 const discount = 10;
 
 //dodaj produkty do tabeli
-const itemContainer = document.querySelector('#items');
+const itemsContainer = document.querySelector("#items");
+let counter = 1;
 
+function addItem(item){
+  itemsContainer.innerHTML += `
+  <tr>
+  <td>${counter++}</td>
+  <td>${item.title}</td>
+  <td>1</td>
+  <td>${item.price}</td>
+  </tr>`;
+}
+addItem(product1);
+addItem(product2);
 // String() , Number(), Boolean()
 if (isNaN(Number(product2.price))) {
   console.log("Podano niepoprawny typ danych")
-}; // Podano niepoprawny typ danych
+} // Podano niepoprawny typ danych
 
-const total = Number(product1.price) + Number(product2.price);
-const totalWithDiscount = total - 10;
+const total = Number(product1.price) + Number(product2.price)
+const totalWithDiscount = total - 10
 
-console.log(document.querySelector('#total-price'))
+console.log(document.querySelector("#total-price"))
 
 console.log(`
   Cena przed zniżką: ${total}
   Cena po zniżce: ${totalWithDiscount};
 `)
-
-
-
-
