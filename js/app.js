3bbb749cfb4a32a19b02ca1ff78ffe7b494d8522
Marcin Rataj <marcinrataj0@gmail.com>
2023-07-05 19:44:47 +0200

    adding new function and input with checkbox, assign function in input, make boolean value so now it work and change with any toggle click

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 const product1 = { price: 10, title: "JS od podstaw" }
 const product2 = { price: "20", title: "PHP od podstaw" }
-const discount = 0;
+const discount = 10;
+let discountEnabled = false;
 
 //dodaj produkty do tabeli
 const itemsContainer = document.querySelector("#items");
 let counter = 1;
+
 function addItem(item) {
   itemsContainer.innerHTML += `
   <tr>
@@ -18,25 +20,29 @@ function addItem(item) {
 addItem(product1)
 addItem(product2)
 
+// dodaj możliwość dodania zniżki
+function addDiscount() {
+discountEnabled = !discountEnabled
+  if (discount > 0) {
+    document.querySelector("#discount-amount").innerHTML = -discount;
+    document.querySelector("#discount").classList.toggle("hidden");
+  }
+  calculatePrice()
+}
 // dodaj zniżkę
 const discountContainer = document.querySelector("#discount-amount")
 
-if (discount > 0) {
-  document.querySelector("#discount-amount").innerHTML = -discount
-  document.querySelector("#discount").classList.remove("hidden")
+
+
+//cena całkowita
+function calculatePrice() {
+  let total = Number(product1.price) + Number(product2.price);
+  //jesli jest true zastosuj zniżkę, inaczej cofnij ją
+  if(discountEnabled){
+    total -= discount;
+  }
+  document.querySelector("#total-price").innerHTML = total;
+
 }
 
-// String() , Number(), Boolean()
-if (isNaN(+product2.price)) {
-  console.log("Podano niepoprawny typ danych")
-}
-
-const total = Number(product1.price) + Number(product2.price);
-const totalWithDiscount = total - discount;
-
-document.querySelector("#total-price").innerHTML = totalWithDiscount;
-
-console.log(`
-  Cena przed zniżką: ${total}
-  Cena po zniżce: ${totalWithDiscount};
-`)
+calculatePrice();
