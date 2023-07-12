42ae26f6983b6d75615d1bf5fc2f85bda9cad9b0
Marcin Rataj <marcinrataj0@gmail.com>
2023-07-13 01:21:35 +0200

    we add change the color of button when you click to add to cart, closures, bind, add class and styling

--- js/app.js
+++ js/app.js
@@ -1,31 +1,49 @@
+
 //definicja elementów
 const coursesList = document.querySelector(".courses-list");
 const counter = document.querySelector(".counter");
 
-//lista produktów
-const items = [];
+function createCart() {
+  //lista produktów
+  const items = [];
+  // funkcja do odświeżania
+  const refreshProductsCount = () => {
+    counter.innerText = items.length;
+  };
+  //funkcja do dodawania
+  const add = (title, price, quantity = 1) => {
+    items.push({ title: title, price: price, quantity: quantity });
+    refreshProductsCount();
+    console.log(items);
+  };
 
-const refreshProductsCount = () => {
-  counter.innerText = items.length;
+  return {
+    add
+  }
 }
 
-const addToCart = (title, price, quantity = 1) => {
-  console.log(title, price, quantity);
-  items.push({
-    title: title,
-    price: price,
-    quantity: quantity,
-  });
-  refreshProductsCount()
-  console.log(items);
-};
+const cart = createCart()
+console.log(cart)
+
+
+// funkcja która obsłuży kolor buttona
+
+const addClass = (className, text) => {
+    return (element) => {
+    element.classList.add(className);
+    element.innerText = text;
+  }
+}
+
+const addClassInCart = addClass('in-cart', 'Dodano');
 
 //obsługa buttona
 const addToCartHandler = (e) => {
   if (e.target.tagName !== "BUTTON") return;
   const title = e.target.dataset.title;
   const price = Number(e.target.dataset.price);
-  addToCart(title, price);
+  cart.add(title, price);
+  addClassInCart(e.target)
 };
 
 //listenery
