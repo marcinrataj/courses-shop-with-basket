74b5842c640624b19e714c866617ccbca85e1bd0
Marcin Rataj <marcinrataj0@gmail.com>
2023-07-12 12:16:52 +0200

    we add new file as cart, we separate page from cart,  styling all in css,  new images, data-sets , parts of cart,  we start adding functions which adding objects to array

--- js/app.js
+++ js/app.js
@@ -1,2 +1,26 @@
-//testing
-//file for new features before they go to the cart.js
+//definicja elementów
+const coursesList = document.querySelector(".courses-list");
+
+//lista produktów
+const items = [];
+
+const addToCart = (title, price, quantity = 1) => {
+  console.log(title, price, quantity);
+  items.push({
+    title: title,
+    price: price,
+    quantity: quantity,
+  });
+  console.log(items);
+};
+
+//obsługa buttona
+const addToCartHandler = (e) => {
+  if (e.target.tagName !== "BUTTON") return;
+  const title = e.target.dataset.title;
+  const price = Number(e.target.dataset.price);
+  addToCart(title, price);
+};
+
+//listenery
+coursesList.addEventListener("click", addToCartHandler);
--- js/cart.js
+++ js/cart.js
@@ -1,103 +1,101 @@
-const cart = {
-  price: 0,
-  getPrice() {
-    this.price = 0;
-    this.items.forEach(item => this.price += item.price)
-    this.price -= this.getDiscountIfEnabled()
-    return this.price;
-  },
-  getDiscount(){
-    return this.discount.amount
-  },
-  getDiscountIfEnabled(){
-    if(this.discount.enabled){
-      return this.getDiscount();
-    }else{
-      return 0;
-    }
-  },
-  discount: {
-    amount: 10,
-    enabled: false,
-  },
-  items: [
-    { price: 10, title: "JS od podstaw" },
-    { price: 20, title: "PHP od podstaw" },
-  ]
-};
+//IIFE
+(function () {
+  const cart = {
+    price: 0,
+    getPrice() {
+      this.price = 0;
+      this.items.forEach((item) => (this.price += item.price));
+      this.price -= this.getDiscountIfEnabled();
+      return this.price;
+    },
+    getDiscount() {
+      return this.discount.amount;
+    },
+    getDiscountIfEnabled() {
+      if (this.discount.enabled) {
+        return this.getDiscount();
+      } else {
+        return 0;
+      }
+    },
+    discount: {
+      amount: 10,
+      enabled: false,
+    },
+    items: [
+      { price: 10, title: "JS od podstaw" },
+      { price: 20, title: "PHP od podstaw" },
+    ],
+  };
 
+  //definicja elementów
+  const discountElement = document.querySelector("#discount");
+  const discountCheckbox = document.querySelector("#add-discount");
+  const itemsContainer = document.querySelector("#items");
 
+  for (const item of cart.items) {
+    addItem(item);
+  }
 
-//definicja elementów
-const discountElement = document.querySelector("#discount");
-const discountCheckbox = document.querySelector("#add-discount");
-// const discountContainer = document.querySelector("#discount-amount");
-const itemsContainer = document.querySelector("#items");
-
-//dodaj produkty do tabeli
-const addItem = (item) => {
-  itemsContainer.innerHTML += `
+  //dodaj produkty do tabeli
+  function addItem(item) {
+    itemsContainer.innerHTML += `
   <tr>
   <td><button class="delete">x</button></td>
   <td>${item.title}</td>
   <td><input class="quantity" type="number" value="1"></td>
   <td>${item.price}</td>
   </tr>`;
-
-}
-// addItem(product1);
-// addItem(product2);
-for (const item of cart.items) {
-  addItem(item)
-}
-
-
-//usuwanie wierszy
-const quantityInputs = document.querySelectorAll(".quantity");
-for (let i = 0; i < quantityInputs.length; i++) {
-  quantityInputs[i].addEventListener("change", removeRowFromQuantity);
-}
-function removeRow(e) {
-  if (e.target.tagName === "BUTTON") {
-    const row = e.target.closest("tr");
-    row.remove();
   }
-}
-function removeRowFromQuantity(e) {
-  if (Number(e.target.value === "0")) {
-    const row = e.target.closest("tr");
-    row.remove();
-  }
-}
+  // addItem(product1);
+  // addItem(product2);
 
-// dodaj zniżkę
-function addDiscount(e) {
-  cart.discount.enabled = e.target.checked;
-  if (cart.getDiscount() > 0) {
-    document
-    .querySelector("#discount-amount")
-    .innerHTML = -cart.getDiscount();
-    discountElement.classList.toggle("hidden");
+  //usuwanie wierszy
+  const quantityInputs = document.querySelectorAll(".quantity");
+  for (let i = 0; i < quantityInputs.length; i++) {
+    quantityInputs[i].addEventListener("change", removeRowFromQuantity);
+  }
+  function removeRow(e) {
+    if (e.target.tagName === "BUTTON") {
+      const row = e.target.closest("tr");
+      row.remove();
+    }
+  }
+  function removeRowFromQuantity(e) {
+    if (Number(e.target.value === "0")) {
+      const row = e.target.closest("tr");
+      row.remove();
+    }
+  }
+
+  // dodaj zniżkę
+  const addDiscount = function (e) {
+    this.discount.enabled = e.target.checked;
+    if (this.getDiscount() > 0) {
+      document.querySelector("#discount-amount").innerHTML =
+        -this.getDiscount();
+      discountElement.classList.toggle("hidden");
+    }
+    calculatePrice();
+  };
+
+  //cena całkowita
+  function calculatePrice() {
+    let total = cart.getPrice();
+    document.querySelector("#total-price").innerHTML = total;
   }
   calculatePrice();
-}
 
-//cena całkowita
-function calculatePrice() {
-  let total = cart.getPrice()
-  document.querySelector("#total-price").innerHTML = total;
-}
-calculatePrice();
+  //listenery, po kliknięciu wywołaj funkcję
+  discountCheckbox.addEventListener("click", addDiscount.bind(cart));
+  itemsContainer.addEventListener("click", removeRow);
 
-//listenery, po kliknięciu wywołaj funkcję
-discountCheckbox.addEventListener("click", addDiscount);
-itemsContainer.addEventListener("click", removeRow);
+  //zaznacz checkbox na początku jeśli trzeba, właściwość dataset 1-true 0-false, możemy zmieniac jak chcemy
+  const discountShouldBeEnabled = Number(
+    discountElement.dataset.discountShouldBeEnabled
+  );
 
-//zaznacz checkbox na początku jeśli trzeba, właściwość dataset 1-true 0-false, możemy zmieniac jak chcemy
-const discountShouldBeEnabled = Number(
-  discountElement.dataset.discountShouldBeEnabled
-);
-
-if (discountShouldBeEnabled) {
-  discountCheckbox.click();
-}
+  if (discountShouldBeEnabled) {
+    discountCheckbox.click();
+  }
+})();
