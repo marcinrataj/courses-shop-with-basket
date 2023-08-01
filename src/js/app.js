ba303a4cc42a5a18da306cef09907f28f21fbb30
Marcin Rataj <marcinrataj0@gmail.com>
2023-08-01 12:44:26 +0200

    correct timer in site to 2024 year count, async / await learn functions setTimeout, setInterval...

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,142 @@
-function calculate (a, b, fn) {
-  function add(a, b){
-    return a + b
-  }
+// // TO JEST PRZYKŁAD WYKONYWANIA KODU NA STRONIE. ROBI SIE BARDZO NIECZYTELNIE
+// // MAMY TUTAJ FUNKCJE Z setTimeoutami, które własnie jako 2-gi parametr maja callback zwracający console.log()
 
-  if(fn === 'add'){
-    return add(a, b);
-  }
-}
+// // sprawdzanie czy produkty są wciąż aktualne
+// // sprawdzanie czy cena jest poprawna
+// // złożenie zamówienia
+// // pobranie promocji ( w podziękowaniu ) do zamówienia
 
-const x = calculate(2, 3, 'add'); console.log(x)
+// const checkProducts = (data, callback) => {
+//   //wysłanie requestu na backend
+//   setTimeout(() => {
+//     callback({status: 'ok'})
+//   }, 2000)
+// }
+// const checkPrice = (data, callback) => {
+//   //wysłanie requestu na backend
+//   setTimeout(() => {
+//     callback({status: 'ok', data})
+//   }, 2000)
+// }
+// const makeOrder = (data, callback) => {
+//   //wysłanie requestu na backend
+//   setTimeout(() => {
+//     callback({orderId: 123, data})
+//   }, 2000)
+// }
+// const checkPromotionForOrder = (orderId, callback) => {
+//   //wysłanie requestu na backend
+//   setTimeout(() => {
+//     callback(['kurs HTML za 50%!'])
+//   }, 2000)
+// }
+
+// const orderData = {}; // dane zamówienia
+// checkProducts(orderData, (response) => {
+//   const {status} = response;
+//   console.log('Czy produkty są dostępne: ', status)
+
+//   checkPrice(orderData, (response2) => {
+//     const {status, order} = response2;
+//     console.log('Cena poprawna: ', status)
+
+//     makeOrder(orderData, (response3) => {
+//       const {orderId, data} = response3;
+//       console.log('Order ID: ', orderId)
+
+//       checkPromotionForOrder(orderId, (response4) => {
+//         const promo = response4;
+//         console.log('Promocje: ', promo)
+//       });
+//     });
+//   });
+// });
+
+// // Piekło callbacków, nie chcemy czegos takiego ^, dostaliśmy cos takiego jak Promisy!!!
+
+//1.==============================================================================
+// callback
+const checkProductsCallback = (data, callback) => {
+  //wysłanie requestu na backend
+  setTimeout(() => {
+    callback({ status: "ok" });
+  }, 2000);
+};
+checkProductsCallback({}, (response) => {
+  console.log("`", response.status, "`", [
+    ` <- Z użyciem jeszcze callbacku, reszta to promisy`,
+  ]);
+});
+
+// promise
+const checkProducts = (data) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ status: "ok" });
+    }, 2000);
+  });
+};
+
+//2.==============================================================================
+const checkPrice = (data) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ status: "ok", data });
+    }, 2000);
+  });
+};
+
+//3.==============================================================================
+const makeOrder = (data) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ orderId: 123, data });
+    }, 2000);
+  });
+};
+
+const checkPromotionForOrder = (orderId) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(["kurs HTML za 50%!"]);
+    }, 2000);
+  });
+};
+
+const orderData = {}; // dane zamowienia
+//promise pattern
+checkProducts(orderData)
+  .then((response) => {
+    console.log("Czy produkty dostępne: ", response.status, [`funkcja checkProducts`]);
+    return checkPrice(orderData);
+  })
+  .then((response) => {
+    console.log("Cena poprawna: ", response.status, [`funkcja checkPrice`]);
+    return makeOrder(orderData);
+  })
+  .then((response) => {
+    console.log("Order ID", response.orderId, [`funkcja makeOrder`]);
+    return checkPromotionForOrder(response.orderId);
+  })
+  .then((response) => {
+    console.log("Promocje: ", response, [`funkcja checkPromotionForOrder`]);
+  })
+  .catch(error => {
+    alert(error)
+  })
+
+  // async / await
+  const asyncFunction = async () => {
+    try{
+    await checkProducts(orderData)
+    await checkPrice(orderData)
+    const response = await makeOrder(orderData);
+    await checkPromotionForOrder(response)
+    console.log('Promocje', response)
+    } catch(error) {
+      alert(error.message)
+    }
+  }
+  asyncFunction()
+  // wszystkie sytuacje await muszą być w funkcji async
+
--- src/js/counter.js
+++ src/js/counter.js
@@ -1,35 +1,44 @@
 (() => {
-  const now = new Date();
-const endTime = now.getTime()
-const counter = document.querySelector('#promotion-counter');
+  const runCounter = () => {
+    const counter = document.querySelector("#promotion-counter");
 
-const getSecondUntilDate = (date) =>{
-  const end = new Date(date)
-  const seconds = end.getTime() / 1000;
-  const startSeconds = Date.now() / 1000;
+    const getSecondUntilDate = (date) => {
+      const end = new Date(date);
+      const seconds = end.getTime() / 1000;
+      const startSeconds = Date.now() / 1000;
 
-  return seconds - startSeconds;
+      return seconds - startSeconds;
+    };
 
-}
+    const getTimerFormat = (seconds) => {
+      if (seconds <= 0) return "Koniec";
+      const h = Math.floor(seconds / 3600);
+      const m = Math.floor((seconds - h * 3600) / 60);
+      const s = Math.floor(seconds - h * 3600 - m * 60);
 
-const getTimerFormat = seconds => {
-  if(seconds <= 0) return 'Koniec'
-  const h = Math.floor(seconds / 3600);
-  const m = Math.floor((seconds - h * 3600) / 60);
-  const s = Math.floor(seconds - h * 3600 - m * 60);
+      return `${h} godz. ${m} min. ${s} s`;
+    };
 
-  return `${h} godz. ${m} min. ${s} s`
-}
+    const s = getSecondUntilDate(endTime) + 12;
+    
+    counter.innerHTML = getTimerFormat(s);
+  };
+  // obliczyc ilość sekund od 0 do czassu docelowego
+  // przygotować odpowiedni wzór licznika (h:m:s)
+  
+  const date = new Date();
+  date.setMinutes(date.getHours() + 123456);
+  // const endTime = date.toISOString()
+  const endTime = '2024-01-24T14:54:00Z';
+  
+  runCounter(endTime)
+  
+  const intervalId = setInterval(() => {
+    runCounter(endTime)
 
-const s = getSecondUntilDate(endTime) + 12
+    if(Date.now() > date.getTime()){
+      clearInterval(intervalId)
+    }
+  }, 1000)
 
-counter.innerHTML = getTimerFormat(s);
-// obliczyc ilość sekund od 0 do czassu docelowego
-// przygotować odpowiedni wzór licznika (h:m:s)
-
-
-
-})()
-
-
-
+})();
